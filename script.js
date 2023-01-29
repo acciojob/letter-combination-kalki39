00e6fdc7ab83e02c0babad7d27f968b995a481d5
kalki39 <120099754+kalki39@users.noreply.github.com>
2023-01-29 20:59:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(D) {
+	const L = {'2':"abc",'3':"def",'4':"ghi",'5':"jkl",
+     '6':"mno",'7':"pqrs",'8':"tuv",'9':"wxyz"};
+  let len = D.length, ans = []
+    if (!len) return []
+    const bfs = (pos, str) => {
+        if (pos === len) ans.push(str)
+        else {
+            let letters = L[D[pos]]
+            for (let i = 0; i < letters.length; i++)
+                bfs(pos+1,str+letters[i])
+        }
+    }
+    bfs(0,"")
+    return ans
+
 }
 
 module.exports = letterCombinations;
